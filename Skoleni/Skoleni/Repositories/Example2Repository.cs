20dc836e6dafd00fb3bf1e66095d6f206924b823
Miroslav Holec <mirek@HOLEC>
2016-12-01 23:18:00 +0100

    Draft pro druhou sadu ukázek

---
+++ Skoleni/Skoleni/Repositories/Example2Repository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Skoleni.Entities;
+
+namespace Skoleni.Repositories
+{
+    /// <summary>
+    /// Ukázka práce s daty, ukládání a change tracking
+    /// </summary>
+    public class Example2Repository
+    {
+        private readonly AppContext _db;
+
+        public Example2Repository(AppContext db)
+        {
+            _db = db;
+        }
+    }
+}
